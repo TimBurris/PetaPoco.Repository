e0f42f3a2c568330d9a15ba45a9fca8ab50779fd
Tim Burris <tim.burris@chalklogic.com>
2020-05-14 09:29:20 -0400

    added a more helpful exception for failing with sql database provider

--- src/PetaPoco.Repository/DefaultSqlDatabaseFactory.cs
+++ src/PetaPoco.Repository/DefaultSqlDatabaseFactory.cs
@@ -14,7 +14,18 @@ namespace PetaPoco.Repository
         }
         public IDatabase Invoke()
         {
-            return new PetaPoco.Database(_connectionString, new Providers.SqlServerDatabaseProvider());
+            try
+            {
+                return new PetaPoco.Database(_connectionString, new Providers.SqlServerDatabaseProvider());
+            }
+            catch (ArgumentException ex)
+            {
+                if (ex.Message != null && ex.Message.Contains("Could not load the SqlServerDatabaseProvider DbProviderFactory"))
+                {
+                    throw new ApplicationException("Error initialiting the SqlServer provider, be sure that you have included System.Data.SqlClient in your project", ex);
+                }
+                throw;
+            }
         }
     }
 }
